108e6e9db4ab53085a3ea35b9dafbac444bab03a
mahmudul-hasan01 <mahamudul2018@gmail.com>
2024-03-27 15:41:08 +0600

    category item are shown using swiperJs

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,11 @@
-import Banner from "./Banner/Banner";
+import Banner from "./Banner";
+import Category from "./Category";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            
+            <Category></Category>
         </div>
     );
 };
