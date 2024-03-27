7b2c197ad95f8fac456a601dcd10422b992b01f0
mahmudul-hasan01 <mahamudul2018@gmail.com>
2024-03-27 20:24:08 +0600

    testimonials are shown and ratings are used

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import Banner from "./Banner";
 import Category from "./Category";
 import Featured from "./Featured";
 import PopularManu from "./PopularManu";
+import Testimonials from "./Testimonials";
 
 const Home = () => {
     return (
@@ -10,6 +11,7 @@ const Home = () => {
             <Category></Category>
             <PopularManu></PopularManu>
             <Featured></Featured>
+            <Testimonials></Testimonials>
         </div>
     );
 };
