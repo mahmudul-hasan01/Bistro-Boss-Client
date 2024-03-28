30e521f1967aeb7e5a5af22d90ab3c0ea81ecd60
mahmudul-hasan01 <mahamudul2018@gmail.com>
2024-03-28 16:42:28 +0600

    dashboard page has been used

--- src/Router.jsx
+++ src/Router.jsx
@@ -6,32 +6,44 @@ import Order from "./Pages/Order/Order";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
 import PrivateRoute from "./PrivateRoute";
+import Dashboard from "./Dashboard/Dashboard";
+import Cart from "./Dashboard/User/Cart";
 
 export const router = createBrowserRouter([
     {
-      path: "/",
-      element: <MainLayout></MainLayout>,
-      children: [
-        {
-            path: '/',
-            element: <Home></Home>
-        },
-        {
-            path: '/menu',
-            element: <Menu></Menu>
-        },
-        {
-            path: '/order/:category',
-            element: <PrivateRoute> <Order></Order> </PrivateRoute>
-        },
-        {
-            path: '/login',
-            element: <Login></Login>
-        },
-        {
-            path: '/register',
-            element: <Register></Register>
-        },
-      ]
+        path: "/",
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/menu',
+                element: <Menu></Menu>
+            },
+            {
+                path: '/order/:category',
+                element: <PrivateRoute> <Order></Order> </PrivateRoute>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
+        ]
     },
-  ]);
+    {
+        path: 'dashboard',
+        element: <PrivateRoute> <Dashboard></Dashboard> </PrivateRoute>,
+        children: [
+            {
+                path: 'cart',
+                element: <Cart></Cart>
+            }
+        ]
+    }
+]);
